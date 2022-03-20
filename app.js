783892a8cfcfd63ddc1fbf4cef4148587f977630
ZsombiTech <horvathzsombor2005@gmail.com>
2022-03-20 18:18:10 +0100

    ..

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
 const tokenverify = require("./middleware/tokenverify");
 const connectdb = require("./config/database");
+
 const app = express();
 
 connectdb();
+
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/", tokenverify, postRoutes);
