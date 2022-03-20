0f2888728f49ad3ae691d3253c856e7e6eee67c2
ZsombiTech <horvathzsombor2005@gmail.com>
2022-03-20 18:04:34 +0100

    ....

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
 const tokenverify = require("./middleware/tokenverify");
-require("dotenv/config");
+const connectdb = require("./config/database");
 const app = express();
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-  console.log("dDd");
-});
-
+connectdb();
 app.use(express.json());
-
 app.use("/auth", authRoutes);
 app.use("/", tokenverify, postRoutes);
 
