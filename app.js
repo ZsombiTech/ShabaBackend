5ef4eae3dedccf34f4f461f110883cf2d3b5ebfa
ZsombiTech <horvathzsombor2005@gmail.com>
2022-04-03 23:49:09 +0200

    h

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 connectdb();
 
